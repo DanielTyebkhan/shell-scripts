b15c3df39d592f8a301836c99f8c3d5f82dbc5d5
Daniel Tyebkhan <dtyebkhan@gmail.com>
2022-01-16 18:47:24 -0500

    Added scripts

---
+++ overleaf.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+google-chrome --profile-directory='Profile 1' https://www.overleaf.com/project
---
+++ quant-tabs.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+google-chrome --profile-directory='Profile 1' gmail.com https://nexus.union.edu/course/view.php?id=19173 https://www.gradescope.com/courses/214987 https://www.overleaf.com/project http://csis.pace.edu/ctappert/cs837-19spring/Rieffel-Quantum%20Computing.pdf
